75d115cd28950b0dd4b128acd7ce4799d1a6e3e3
Henry <cryptix@riseup.net>
2019-09-14 17:49:09 +0200

    don't spawn ssb-server
    start go-sbot by yourself

--- src/pages/models/lib/cooler.js
+++ src/pages/models/lib/cooler.js
@@ -5,31 +5,6 @@ const secretStack = require('secret-stack')
 const ssbConfig = require('ssb-config')
 const debug = require('debug')('oasis')
 
-const server = secretStack()
-
-server
-  .use(require('ssb-db'))
-  .use(require('ssb-replicate'))
-
-  .use(require('ssb-about'))
-  .use(require('ssb-backlinks'))
-  .use(require('ssb-blobs'))
-  .use(require('ssb-ebt'))
-  .use(require('ssb-friends'))
-  .use(require('ssb-gossip'))
-  .use(require('ssb-invite'))
-  .use(require('ssb-links'))
-  .use(require('ssb-local'))
-  .use(require('ssb-logging'))
-  .use(require('ssb-master'))
-  .use(require('ssb-no-auth'))
-  .use(require('ssb-onion'))
-  .use(require('ssb-ooo'))
-  .use(require('ssb-plugins'))
-  .use(require('ssb-query'))
-  .use(require('ssb-unix-socket'))
-  .use(require('ssb-ws'))
-
 const rawConnect = () => new Promise((resolve, reject) => {
   ssbClient((err, api) => {
     if (err) {
@@ -75,16 +50,17 @@ const handle = new Promise((resolve, reject) => {
     debug('Initial connection attempt failed')
     debug('Starting Scuttlebutt server')
 
-    server(ssbConfig)
-
+    
     const connectOrRetry = () => {
-      rawConnect().then((ssb) => {
-        debug('Retrying connection to own server')
-        resolve(ssb)
-      }).catch((e) => {
-        debug(e)
-        connectOrRetry()
-      })
+      setTimeout(() => {
+        rawConnect().then((ssb) => {
+          debug('Retrying connection to own server')
+          resolve(ssb)
+        }).catch((e) => {
+          debug(e)
+          connectOrRetry()
+        })
+      }, 1000)
     }
 
     connectOrRetry()
